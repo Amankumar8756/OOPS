9d33f4529639dbb159088e68d44b965a150aa7f7
Aman kumar <iit2022168@iiita.ac.in>
2025-07-24 06:00:14 +0530

    shallow copy

--- loveBubber/basic.cpp
+++ loveBubber/basic.cpp
@@ -1,28 +1,37 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 class Hero
 {
-    // properties------
 private:
     int health;
 
 public:
+    char *name;  // Fixed size array for name
     char level;
-    int age ;
+    int age;
 
+    // Parameterized constructor
+    Hero(int health, char level)
+    {
+        this->health = health;
+        this->level = level;
+    }
 
-Hero(int health){
-    cout<<"this -> "<<this<<endl;
-    this->health = health;
-}
+    // Default constructor
+    Hero()
+    {
+        cout << "I am a constructor " << endl;
+    }
 
-// default constructors-------
-     Hero(){
-        cout<<"i am a constructor "<<endl;
-     }
-
+    void print()
+    {
+        cout << endl;
+        cout << "[ Name: " << this->name << " , ";
+        cout << "Health: " << this->health << " , ";
+        cout << "Level: " << this->level << " ]" << endl;
+        cout << endl;
+    }
 
     int getHealth()
     {
@@ -33,30 +42,71 @@ Hero(int health){
     {
         health = h;
     }
+
+    void setLevel(char level)
+    {
+        this->level = level;
+    }
+
+    void setName(char name[])
+    {
+        strcpy(this->name, name);
+    }
 };
 
 int main()
 {
+    Hero hero1; // Calls default constructor
+
+    hero1.setHealth(22);
+    hero1.setLevel('A');
+    char name[7] = "AmanK";  // Fixed name
+    hero1.setName(name);
+
+    hero1.print(); // Display hero details
+
+    // Use default copy constructor
+
+   
+    Hero hero2(hero1);  // Copy hero1 into hero2
+    hero2.print();
+
+
+     hero1.name[0]='M';
+     hero1.print();
+
+     hero2.print();
+
+
+
+
+
+
+
+
+
+
+
+// Hero aman(70, 'c');
+
+// aman.print();
+
+// // copy constructor 
+// Hero mota(aman);
+
+// mota.print();
+
+
     // creation of objects  -- statically
 
-    Hero aman(10); 
+    // Hero aman;
+    // aman.print();
 
-    cout<<"adderess of aman -> " <<&aman<<endl;
-   
-     // dynamically  allocations---
- 
-    Hero *h = new Hero(22);
+    // cout<<"adderess of aman -> " <<&aman<<endl;
 
+    //  // dynamically  allocations---
 
-
-
-
-
-
-
-
-
-
+    // Hero *h = new Hero(22);
 
     // // dynamically  allocations---
 
