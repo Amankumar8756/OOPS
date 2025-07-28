096a00932090e84ac7bf360ee687191f353ddf84
Aman kumar <iit2022168@iiita.ac.in>
2025-07-28 07:30:55 +0530

    encapsulation_done

---
+++ loveBubber/encapsulation.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Student{
+
+    private:
+
+    string str ;
+    int num;
+
+    public:
+
+    int getNum(){
+        return this->num;
+    }
+};
+
+int main(){
+
+    Student first;
+
+    cout<<" sb shi challrha hai= "<<endl;
+}
